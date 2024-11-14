acd59a36b8335197ebf6439ba16a4315212717fc
index-git <sileny.index@gmail.com>
2024-11-14 11:29:00 +0100

    Add Black

--- src/python/main.py
+++ src/python/main.py
@@ -2,24 +2,31 @@ from fastapi import FastAPI
 
 from . import contacts
 
+
 def singleton(class_):
     instances = {}
+
     def getinstance(*args, **kwargs):
         if class_ not in instances:
             instances[class_] = class_(*args, **kwargs)
         return instances[class_]
+
     return getinstance
+
 
 @singleton
 class PhonebookSingleton(contacts.Phonebook):
     pass
 
+
 app = FastAPI()
 
+
 @app.get("/phonebook/")
-def search_contact(prefix: contacts.Name=''):
+def search_contact(prefix: contacts.Name = ""):
     phonebook = PhonebookSingleton({})
     return phonebook.search_contact(prefix)
+
 
 @app.post("/phonebook/")
 def add_contact(name: contacts.Name, phone: contacts.PhoneNumber):
--- src/python/test_contacts.py
+++ src/python/test_contacts.py
@@ -2,66 +2,101 @@ import pytest
 from .contacts import Phonebook
 
 
-@pytest.mark.parametrize('base_phonebook, expected_phonebook', [
-    ({'123456789': 'a'}, {'subtree': {'a': {'phone': '123456789'}},}, ),
-    ({'123456789': 'a', '1': 'b'}, {'subtree': {'a': {'phone': '123456789'}, 'b': {'phone': '1'}}, },),
-    ({'123456789': 'a', '1': 'b', '2345': 'ad'},
-     {'subtree':
-          {'a': {'phone': '123456789',
-                 'subtree': {'d': {'phone': '2345'}}},
-           'b': {'phone': '1'}},
-      },
-     ),
-])
+@pytest.mark.parametrize(
+    "base_phonebook, expected_phonebook",
+    [
+        (
+            {"123456789": "a"},
+            {
+                "subtree": {"a": {"phone": "123456789"}},
+            },
+        ),
+        (
+            {"123456789": "a", "1": "b"},
+            {
+                "subtree": {"a": {"phone": "123456789"}, "b": {"phone": "1"}},
+            },
+        ),
+        (
+            {"123456789": "a", "1": "b", "2345": "ad"},
+            {
+                "subtree": {
+                    "a": {"phone": "123456789", "subtree": {"d": {"phone": "2345"}}},
+                    "b": {"phone": "1"},
+                },
+            },
+        ),
+    ],
+)
 def test_contacts_init(base_phonebook, expected_phonebook):
     phonebook = Phonebook(base_phonebook)
     assert phonebook.prefixed_phonebook == expected_phonebook
 
 
-@pytest.mark.parametrize('base_phonebook, name, number, expected_phonebook, expected_result', [
-    ({'123456789': 'a'},
-     'b',
-     '1',
-     {'subtree': {'a': {'phone': '123456789'}, 'b': {'phone': '1'}}, },
-     True,
-     ),
-    ({'123456789': 'a'},
-     'a',
-     '123456789',
-     {'subtree': {'a': {'phone': '123456789'}}, },
-     False,
-     ),
-    ({'123456789': 'a'},
-     'ad',
-     '789',
-     {'subtree':
-          {'a': {'phone': '123456789',
-                 'subtree': {'d': {'phone': '789'}},
-           }},
-      },
-     True,
-     ),
-])
+@pytest.mark.parametrize(
+    "base_phonebook, name, number, expected_phonebook, expected_result",
+    [
+        (
+            {"123456789": "a"},
+            "b",
+            "1",
+            {
+                "subtree": {"a": {"phone": "123456789"}, "b": {"phone": "1"}},
+            },
+            True,
+        ),
+        (
+            {"123456789": "a"},
+            "a",
+            "123456789",
+            {
+                "subtree": {"a": {"phone": "123456789"}},
+            },
+            False,
+        ),
+        (
+            {"123456789": "a"},
+            "ad",
+            "789",
+            {
+                "subtree": {
+                    "a": {
+                        "phone": "123456789",
+                        "subtree": {"d": {"phone": "789"}},
+                    }
+                },
+            },
+            True,
+        ),
+    ],
+)
 def test_add_contact(base_phonebook, name, number, expected_phonebook, expected_result):
     phonebook = Phonebook(base_phonebook)
     result = phonebook.add_contact(name, number)
     assert result == expected_result
     assert phonebook.prefixed_phonebook == expected_phonebook
 
-@pytest.mark.parametrize('base_phonebook, name_prefix, expected_phones', [
-    ({'123456789': 'a', '1': 'b', '2345': 'ad'},
-     'a',
-      ['123456789', '2345'],
-     ),
-    ({'123456789': 'a', '1': 'b', '2345': 'ad'},
-     'x',
-     [],
-     ),
-    ({'123456789': 'a', '1': 'b', '2345': 'ad'},
-     'ad',
-     ['2345'],
-     ),
-])
+
+@pytest.mark.parametrize(
+    "base_phonebook, name_prefix, expected_phones",
+    [
+        (
+            {"123456789": "a", "1": "b", "2345": "ad"},
+            "a",
+            ["123456789", "2345"],
+        ),
+        (
+            {"123456789": "a", "1": "b", "2345": "ad"},
+            "x",
+            [],
+        ),
+        (
+            {"123456789": "a", "1": "b", "2345": "ad"},
+            "ad",
+            ["2345"],
+        ),
+    ],
+)
 def test_search_contact(base_phonebook, name_prefix, expected_phones):
     phonebook = Phonebook(base_phonebook)
     assert phonebook.search_contact(name_prefix) == expected_phones
