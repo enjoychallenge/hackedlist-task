98d2560c5422e27b9e4aad0bf0344b41b69a725d
Jiri Kozel <jirik@users.noreply.github.com>
2024-11-15 13:09:16 +0100

    Add js code style check + format the code

--- src/js/src/components/Insert.tsx
+++ src/js/src/components/Insert.tsx
@@ -1,26 +1,26 @@
-import styles from "@/styles/Insert.module.css";
-import {useRef, useState, MouseEvent} from "react";
+import styles from '@/styles/Insert.module.css'
+import { useRef, useState, MouseEvent } from 'react'
 
 interface InsertProps {
-  contactsEndpoint: string;
+  contactsEndpoint: string
 }
 
 interface Error422Item {
-  loc: string[],
-  msg: string,
+  loc: string[]
+  msg: string
 }
 
 interface ErrorMessages {
-  name?: string,
-  phone?: string,
+  name?: string
+  phone?: string
 }
 
 export default function Insert(props: InsertProps) {
-  const [name, setName] = useState<string>("");
-  const [phone, setPhone] = useState<string>("");
-  const [result, setResult] = useState<boolean | null>(null);
+  const [name, setName] = useState<string>('')
+  const [phone, setPhone] = useState<string>('')
+  const [result, setResult] = useState<boolean | null>(null)
   const [errorMsgs, setErrorMsgs] = useState<ErrorMessages>({})
-  const insertingData = useRef<object | null>(null);
+  const insertingData = useRef<object | null>(null)
 
   const handleButtonClick = async (evt: MouseEvent<HTMLElement>) => {
     evt.preventDefault()
@@ -35,66 +35,76 @@ export default function Insert(props: InsertProps) {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data)
-    });
-    if(resp.ok) {
+      body: JSON.stringify(data),
+    })
+    if (resp.ok) {
       const res = await resp.json()
-      if(insertingData.current === data) {
-        insertingData.current = null;
+      if (insertingData.current === data) {
+        insertingData.current = null
         setResult(res)
       }
     } else {
       const newErrorMsgs: ErrorMessages = {}
-      if(resp.status === 422) {
+      if (resp.status === 422) {
         const res = await resp.json()
         res.detail.forEach((errDetail: Error422Item) => {
-          if(JSON.stringify(errDetail.loc) === JSON.stringify(['body', 'name'])) {
+          if (
+            JSON.stringify(errDetail.loc) === JSON.stringify(['body', 'name'])
+          ) {
             newErrorMsgs.name = errDetail.msg
           }
-          if(JSON.stringify(errDetail.loc) === JSON.stringify(['body', 'phone'])) {
+          if (
+            JSON.stringify(errDetail.loc) === JSON.stringify(['body', 'phone'])
+          ) {
             newErrorMsgs.phone = errDetail.msg
           }
         })
       }
-      if(insertingData.current === data) {
-        insertingData.current = null;
+      if (insertingData.current === data) {
+        insertingData.current = null
         setResult(false)
         setErrorMsgs(newErrorMsgs)
       }
     }
   }
 
-  let resultJsx = null;
+  let resultJsx = null
   if (result !== null) {
-    resultJsx = result ? <div>Data successfully inserted!</div> : <div className="warn">Data was not inserted!</div>
+    resultJsx = result ? (
+      <div>Data successfully inserted!</div>
+    ) : (
+      <div className="warn">Data was not inserted!</div>
+    )
   }
 
   return (
-      <form className={styles.insert}>
-        <div>
-          <h3>Name</h3>
-          <input type="text"
-                 onChange={(e) => {
-                   setResult(null)
-                   setName(e.target.value)
-                 }}
-                 value={name}
-          />
-          {errorMsgs.name ? <div className='warn'>{errorMsgs.name}</div> : null}
-        </div>
-        <div>
-          <h3>Phone number</h3>
-          <input type="text"
-                 onChange={(e) => {
-                   setResult(null)
-                   setPhone(e.target.value)
-                 }}
-                 value={phone}
-          />
-          {errorMsgs.phone ? <div className='warn'>{errorMsgs.phone}</div> : null}
-        </div>
-        {resultJsx}
-        <button onClick={handleButtonClick}>Insert</button>
-      </form>
-  );
+    <form className={styles.insert}>
+      <div>
+        <h3>Name</h3>
+        <input
+          type="text"
+          onChange={(e) => {
+            setResult(null)
+            setName(e.target.value)
+          }}
+          value={name}
+        />
+        {errorMsgs.name ? <div className="warn">{errorMsgs.name}</div> : null}
+      </div>
+      <div>
+        <h3>Phone number</h3>
+        <input
+          type="text"
+          onChange={(e) => {
+            setResult(null)
+            setPhone(e.target.value)
+          }}
+          value={phone}
+        />
+        {errorMsgs.phone ? <div className="warn">{errorMsgs.phone}</div> : null}
+      </div>
+      {resultJsx}
+      <button onClick={handleButtonClick}>Insert</button>
+    </form>
+  )
 }
--- src/js/src/components/Search.tsx
+++ src/js/src/components/Search.tsx
@@ -1,80 +1,82 @@
-import styles from "@/styles/Search.module.css";
-import {useEffect, useRef, useState} from "react";
+import styles from '@/styles/Search.module.css'
+import { useEffect, useRef, useState } from 'react'
 
 interface FoundPhonesProps {
-  foundPhones: string[];
+  foundPhones: string[]
 }
 
 const FoundPhones = (props: FoundPhonesProps) => {
-  if(props.foundPhones.length > 0) {
-    return <>
-      <h3>Found phone numbers</h3>
-      <ul>
-          {
-            props.foundPhones.map((foundPhone, idx) => {
-              return <li key={idx}>{foundPhone}</li>
-            })
-          }
-      </ul>
-    </>
+  if (props.foundPhones.length > 0) {
+    return (
+      <>
+        <h3>Found phone numbers</h3>
+        <ul>
+          {props.foundPhones.map((foundPhone, idx) => {
+            return <li key={idx}>{foundPhone}</li>
+          })}
+        </ul>
+      </>
+    )
   } else {
     return <div className="warn">No phone numbers found!</div>
   }
 }
 
 interface SearchProps {
-  contactsEndpoint: string;
+  contactsEndpoint: string
 }
 
 export default function Search(props: SearchProps) {
-  const [searchText, setSearchText] = useState<string>("");
-  const requestingText = useRef<string | null>(null);
-  const [foundPhones, setFoundPhones] = useState<string[]>([]);
+  const [searchText, setSearchText] = useState<string>('')
+  const requestingText = useRef<string | null>(null)
+  const [foundPhones, setFoundPhones] = useState<string[]>([])
 
   useEffect(() => {
     if (searchText.length > 0) {
-
       const urlParams = new URLSearchParams({
         prefix: searchText,
       }).toString()
 
       requestingText.current = searchText
-      fetch(`${props.contactsEndpoint}?${urlParams}`)
-          .then(async (response) => {
-            if(requestingText.current !== searchText) {
-              return
-            }
-            if (response.ok) {
-              const phones = await response.json()
-              if (requestingText.current !== searchText) {
-                return
-              }
-              requestingText.current = null;
-              setFoundPhones(phones)
-            } else {
-              requestingText.current = null;
-              setFoundPhones([])
-            }
-          })
+      fetch(`${props.contactsEndpoint}?${urlParams}`).then(async (response) => {
+        if (requestingText.current !== searchText) {
+          return
+        }
+        if (response.ok) {
+          const phones = await response.json()
+          if (requestingText.current !== searchText) {
+            return
+          }
+          requestingText.current = null
+          setFoundPhones(phones)
+        } else {
+          requestingText.current = null
+          setFoundPhones([])
+        }
+      })
     } else {
       setFoundPhones([])
     }
-  }, [searchText]);
+  }, [searchText, props.contactsEndpoint])
 
   let results = <>Loading data ...</>
   if (requestingText.current === null) {
-     results = searchText.length > 0 ? <FoundPhones foundPhones={foundPhones} /> : <>Type at least 1 letter.</>
+    results =
+      searchText.length > 0 ? (
+        <FoundPhones foundPhones={foundPhones} />
+      ) : (
+        <>Type at least 1 letter.</>
+      )
   }
 
   return (
-      <div className={styles.search}>
-        <input type="text"
-               onChange={(e) => setSearchText(e.target.value)}
-               value={searchText}
-        />
-        <div>
-          {results}
-        </div>
-      </div>
-  );
+    <div className={styles.search}>
+      <input
+        type="text"
+        onChange={(e) => setSearchText(e.target.value)}
+        value={searchText}
+      />
+      <div>{results}</div>
+    </div>
+  )
 }
